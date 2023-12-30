02c94b79ee8a9af0c1a7fef0f6242c4abd096a55
Bruce <36781800+real-bruce@users.noreply.github.com>
2023-12-30 16:10:47 +0800

    feat: add java tools bigDecimal utils

---
+++ tools-java/src/main/java/com/bruce/Main.java
@@ -0,0 +1,12 @@
+package com.bruce;
+
+/**
+ * @author Bruce
+ * @create 2023/12/30
+ * @deprecated
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
